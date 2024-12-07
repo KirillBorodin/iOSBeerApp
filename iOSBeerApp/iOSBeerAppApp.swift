716948ed62466faa770585f5d0fb06e60b919464
Kyrylo Borodin <kborodin.dev@gmail.com>
2024-12-07 10:20:50 +0200

    Initial Commit

---
+++ iOSBeerApp/iOSBeerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSBeerAppApp.swift
+//  iOSBeerApp
+//
+//  Created by Kirill on 07.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSBeerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
